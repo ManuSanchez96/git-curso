c0892fbfc4361688eb58d3ea485e6acff48d3762
Manu <1615576@alu.murciaeduca.es>
2023-05-19 19:12:00 +0200

    mi primer commit

---
+++ Prueba_Git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba_git;
+
+/**
+ *
+ * @author Manuel Jose Sanchez Lopez
+ */
+public class Prueba_Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
